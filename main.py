f4a3cd00f3d784fe6e6e34f7c4e676fdcdc845f9
Will Blew <willblew@gmail.com>
2015-03-26 02:56:58 +0000

    Fix typos and misnaming add junk

--- main.py
+++ main.py
@@ -11,18 +11,18 @@ class Autode():
 		self.stdin_path = '/dev/null'
 		self.stdout_path = '/dev/tty'
 		self.stderr_path = '/dev/tty'
-		self.pidfile_path == '/var/run/Autode/Autode.pid'
+		self.pidfile_path = '/var/run/Autode/Autode.pid'
 		self.pidfile_timeout = 10
 
 	def run(self):
-		white True:
+		while True:
 			# Do all of the things, well kind of. We'll be sure to modulize an such
 
 			# Please to be logging
-			logger.debug("DEBUG: ")
-			logger.info("INFO: ")
-			logger.warn("WARNING: ")
-			logger.error("ERROR: ")
+			log.debug("DEBUG: ")
+			log.info("INFO: ")
+			log.warn("WARNING: ")
+			log.error("ERROR: ")
 			time.sleep(5)
 autode = Autode()
 log = logging.getLogger("AutodeLog")
